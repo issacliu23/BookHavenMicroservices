3e901e5254b596c7d2cee2754e844599739ba214
Issac Liu <issac.liu.work@gmail.com>
2021-09-09 22:29:39 +0800

    Restructured folders, renamed services and set ports

---
+++ reviewservice/src/test/java/com/ncs/issnus/reviewservice/ReviewserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ncs.issnus.reviewservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
